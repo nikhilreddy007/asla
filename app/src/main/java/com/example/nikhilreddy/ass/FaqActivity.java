b818d9e83685cce69f685ee0b0f8399be52f5c6e
Nikhil Reddy <33191229+nikhilreddy007@users.noreply.github.com>
2018-10-15 12:15:27 +0530

    faq done

--- app/src/main/java/com/example/nikhilreddy/ass/FaqActivity.java
+++ app/src/main/java/com/example/nikhilreddy/ass/FaqActivity.java
@@ -36,11 +36,31 @@ public class FaqActivity extends AppCompatActivity implements SearchView.OnQuery
         expandableListView = (ExpandableListView) findViewById(R.id.expListViewfaq);
         expandableListDetail = new LinkedHashMap<String, List<String>>();
 
-        List<String> chap1 = new ArrayList<String>();
-        int res = getResources().getIdentifier("m0_2", "string", this.getPackageName());
-        chap1.add(getResources().getString(res));
+        List<String> q1 = new ArrayList<String>();
+        q1.add(getResources().getString(getResources().getIdentifier("a1", "string", this.getPackageName())));
+        List<String> q2 = new ArrayList<String>();
+        q2.add(getResources().getString(getResources().getIdentifier("a2", "string", this.getPackageName())));
+        List<String> q3 = new ArrayList<String>();
+        q3.add(getResources().getString(getResources().getIdentifier("a3", "string", this.getPackageName())));
+        List<String> q4 = new ArrayList<String>();
+        q4.add(getResources().getString(getResources().getIdentifier("a4", "string", this.getPackageName())));
+        List<String> q5 = new ArrayList<String>();
+        q5.add(getResources().getString(getResources().getIdentifier("a5", "string", this.getPackageName())));
+        List<String> q6 = new ArrayList<String>();
+        q6.add(getResources().getString(getResources().getIdentifier("a6", "string", this.getPackageName())));
+        List<String> q7 = new ArrayList<String>();
+        q7.add(getResources().getString(getResources().getIdentifier("a7", "string", this.getPackageName())));
+        List<String> q8 = new ArrayList<String>();
+        q8.add(getResources().getString(getResources().getIdentifier("a8", "string", this.getPackageName())));
 
-        expandableListDetail.put("1. What is Autosar?", chap1);
+        expandableListDetail.put("How does AUTOSAR secure interoperability of solutions in the market", q1);
+        expandableListDetail.put("What are the main drivers for the Adaptive Platform?", q2);
+        expandableListDetail.put("When do I use the Classic Platform? When do I use the Adaptive Platform?", q3);
+        expandableListDetail.put("How does AUTOSAR keep the standard up to date?", q4);
+        expandableListDetail.put("Does AUTOSAR consider standardization of application interfaces, e.g. interface to brake-system?", q5);
+        expandableListDetail.put("Migration: How does the integration of non-AUTOSAR compliant basic software work?", q6);
+        expandableListDetail.put("What are the performance requirements on the controller to support an AUTOSAR ECU?", q7);
+        expandableListDetail.put("Does AUTOSAR use existing standards?", q8);
 
 
         expandableListTitle = new ArrayList<String>(expandableListDetail.keySet());
